906c723b4d6008e0696a703831ba32a532986b17
Dongho Seo <ehcws333@gmail.com>
2023-08-27 13:52:33 +0900

    chore: dummy data added

---
+++ src/lib/types/data-type.ts
@@ -0,0 +1,28 @@
+export interface VectorDataType {
+    articlePosition: number[];
+    articleID: string
+    clusterID: string
+}
+
+export interface ArticleType {
+    articleID: string;
+    articleDate: Date;
+    articlePopularity: number;
+    articleProvider: string;
+    articleTitle: string;
+    articleContent: string;
+    articleProblem: string;
+    articleCause: string;
+    articleSolution: string;
+}
+
+export interface ClusterType {
+    timeframeID: number;
+    clusterID: number;
+    clusterWrap: [number, number][];
+    clusterCenter: [number, number];
+    clusterTitle: string;
+    clusterProblem: string;
+    clusterCause: string;
+    clusterSolution: string;
+}
